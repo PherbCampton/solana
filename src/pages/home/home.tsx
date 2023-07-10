84e1140ea4db7b2bb4644f5fa942a1f67d706280
Samuel Tuinperi <tuinperisamuel@yahoo.com>
2023-07-10 16:16:12 +0100

    removed unused variable

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,6 @@
 import { Fragment } from "react";
 import Banner from "../../components/Banner/Banner";
 import Hero from "../../components/Hero/Hero";
-import Nav from "../../components/Nav/Nav";
 
 export const Home = () => {
   return (
